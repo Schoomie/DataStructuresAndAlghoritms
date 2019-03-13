f0138f9cd402891b0a777d7c811e41bd4372bd7b
Schoomie <jakubpatecki97@gmail.com>
2019-03-13 13:09:58 +0100

    comparable <T>

--- TheaterSeats/Main.java
+++ TheaterSeats/Main.java
@@ -4,10 +4,19 @@ package TheaterSeats;
  * Created by Kuba on 13/03/2019.
  */
 public class Main {
-public static void main(String [] args){
+    public static void main(String[] args) {
+        Theatre theatre = new Theatre("Teatr Juliusza Słowackiego", 8, 12);
 
 
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Pay for your Seat.");
+        } else {
+            System.out.println("Seat is reserved.");
+        }
+
+
+    }
 }
 
 
-}
+
